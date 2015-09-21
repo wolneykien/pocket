d216a005cea5c7dfbee4c73db60133a122b447b5
Paul Wolneykien <manowar@altlinux.org>
2015-09-21 19:47:28 +0300

    Pocket 0.2.11
    Fix: allow to rebuild a particular package version (via name=ver syntax).

--- pocket/pocket-functions.sh
+++ pocket/pocket-functions.sh
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-POCKET_VERSION="0.2.10"
+POCKET_VERSION="0.2.11"
 PROG_VERSION="$POCKET_VERSION"
 
 CONFDIR="/etc/pocket"
